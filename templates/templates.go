60a8a056ac65931db27a9eb9e600f88908be6ccb
Brandon Sprague <brandon@sprague.mx>
2025-06-01 20:14:28 -0700

    Work on MJML template

--- templates/templates.go
+++ templates/templates.go
@@ -10,36 +10,64 @@ import (
 
 	"postmark-inbound/claude"
 	"postmark-inbound/tosdr"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 //go:embed *.mjml
 var templateFiles embed.FS
 
 type EmailTemplateData struct {
-	Subject       string
-	Company       string
-	PolicyType    string
-	Confidence    string
-	HasToSDR      bool
-	ToSDRServices []tosdr.SearchService
+	Subject    string
+	Company    string
+	PolicyType string
+
+	DeltaReport   *DeltaReport
+	SummaryReport *SummaryReport
+	ToSDR         *ToSDR
 }
 
-func GenerateMJML(classification *claude.PolicyClassification, tosDRResults *tosdr.SearchResponse) (string, error) {
+type DeltaReport struct {
+	PrevDate string
+	YourDate string
+
+	Points []SummaryPoint
+}
+
+type SummaryReport struct {
+	Points []SummaryPoint
+}
+
+type SummaryPoint struct {
+	Text string
+}
+
+type ToSDR struct {
+	Points []ToSDRPoint
+}
+
+type ToSDRPoint struct {
+	Title          string
+	Source         string
+	Classification string
+}
+
+func GenerateMJML(classification *claude.PolicyClassification, svc *tosdr.Service) (string, error) {
 	tmpl, err := template.ParseFS(templateFiles, "email.mjml")
 	if err != nil {
 		return "", fmt.Errorf("error parsing template: %v", err)
 	}
 
-	data := EmailTemplateData{
-		Subject:    fmt.Sprintf("Policy Change Summary: %s", classification.Company),
-		Company:    classification.Company,
-		PolicyType: strings.Title(strings.ReplaceAll(classification.PolicyType, "_", " ")),
-		Confidence: strings.Title(classification.Confidence),
-		HasToSDR:   tosDRResults != nil && len(tosDRResults.Services) > 0,
-	}
+	title := cases.Title(language.English)
 
-	if data.HasToSDR {
-		data.ToSDRServices = tosDRResults.Services
+	data := EmailTemplateData{
+		Subject:       fmt.Sprintf("Policy Change Summary: %s", classification.Company),
+		Company:       classification.Company,
+		PolicyType:    title.String(strings.ReplaceAll(classification.PolicyType, "_", " ")),
+		DeltaReport:   &DeltaReport{},   // TODO: Pass in the right stuff so we know when to show this
+		SummaryReport: &SummaryReport{}, // TODO: Pass in the right stuff so we know when to show this
+		ToSDR:         toToSDR(svc),
 	}
 
 	var buf bytes.Buffer
@@ -48,6 +76,25 @@ func GenerateMJML(classification *claude.PolicyClassification, tosDRResults *tos
 	}
 
 	return buf.String(), nil
+}
+
+func toToSDR(svc *tosdr.Service) *ToSDR {
+	if svc == nil {
+		return nil
+	}
+
+	points := make([]ToSDRPoint, 0, len(svc.Points))
+	for _, p := range svc.Points {
+		points = append(points, ToSDRPoint{
+			Title:          p.Title,
+			Source:         p.Source,
+			Classification: p.Case.Classification,
+		})
+	}
+
+	return &ToSDR{
+		Points: points,
+	}
 }
 
 func CompileMJMLToHTML(mjmlContent string) (string, error) {
